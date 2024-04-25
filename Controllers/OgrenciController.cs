34e868929e373ef467fd66d7efaa02c99dd5c255
CyberWarmonger <CyberWarmonger@CYBERWARMONGER>
2024-04-25 17:51:33 +0300

    Add project files.

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Vize_Projesi.Models.Entity;
+
+namespace Mvc_Vize_Projesi.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        vize_mvcEntities db = new vize_mvcEntities();
+        // GET: Ogrenci
+        public ActionResult Index()
+        {
+            var ogrenciler = db.Tbl_Ogrenci.ToList();
+            return View(ogrenciler);
+        }
+    }
+}
---
+++ Controllers/OgretmenController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Vize_Projesi.Models.Entity;
+
+
+namespace Mvc_Vize_Projesi.Controllers
+{
+    public class OgretmenController : Controller
+    {
+        // GET: Ogretmen
+        vize_mvcEntities db = new vize_mvcEntities();
+        public ActionResult Index()
+        {
+            var ogretmenler = db.Tbl_Ogretmen.ToList();
+            return View(ogretmenler);
+        }
+    }
+}
