f4175a085b14b7945357f70a10af71887862e29f
CyberWarmonger <CyberWarmonger@CYBERWARMONGER>
2024-04-27 21:35:10 +0300

    Raporlar Sayfasi Olusturuldu Mezun Ogrecniler Bu Sayfaya Tasindi

---
+++ Controllers/RaporlarController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Vize_Projesi.Models.Entity;
+
+
+namespace Mvc_Vize_Projesi.Controllers
+{
+    public class RaporlarController : Controller
+    {
+        vize_mvcEntities2 db = new vize_mvcEntities2();
+
+        // GET: Raporlar
+        public ActionResult Index()
+        {
+            var rapor = db.Tbl_Ogrenci.ToList();
+            return View(rapor);
+        }
+    }
+}
